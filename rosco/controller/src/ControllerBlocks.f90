a8c2ca947550068db42554498890a6d48e4a779d
Joeri Frederik <joeri.frederik@nrel.gov>
2026-01-27 13:15:07 -0700

    Working mode 7 indentical to branch awc_cl_tsr

--- rosco/controller/src/ControllerBlocks.f90
+++ rosco/controller/src/ControllerBlocks.f90
@@ -506,7 +506,7 @@ CONTAINS
         IF ( CntrPar%SS_Mode == 1) THEN
             ! Find setpoint shift amount
             R_Total = LocalVar%PRC_R_Speed * LocalVar%PRC_R_Torque * LocalVar%PRC_R_Pitch
-            DelOmega = ((LocalVar%BlPitchCMeas - LocalVar%PC_MinPit)/0.524) * CntrPar%SS_VSGain - ((CntrPar%VS_RtPwr * R_Total - LocalVar%VS_LastGenPwr))/CntrPar%VS_RtPwr * CntrPar%SS_PCGain ! Normalize to 30 degrees for now
+            DelOmega = ((LocalVar%PC_PitComT - LocalVar%PC_MinPit)/0.524) * CntrPar%SS_VSGain - ((CntrPar%VS_RtPwr * R_Total - LocalVar%VS_LastGenPwr))/CntrPar%VS_RtPwr * CntrPar%SS_PCGain ! Normalize to 30 degrees for now
             DelOmega = DelOmega * CntrPar%PC_RefSpd
             ! Filter
             LocalVar%SS_DelOmegaF = LPFilter(DelOmega, LocalVar%DT, CntrPar%F_SSCornerFreq, LocalVar%FP, LocalVar%iStatus, LocalVar%restart, objInst%instLPF) 
